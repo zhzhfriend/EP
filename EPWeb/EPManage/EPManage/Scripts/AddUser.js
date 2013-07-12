6373994742442ff10968b8f1964510d0e47c42dc
wanglifeng <tytc011@163.com>
2013-07-13 01:26:49 +0800

    fix bugs

---
+++ EPWeb/EPManage/EPManage/Scripts/AddUser.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#btnAddUser').click(function () {
+        $.post('/User/Add', { 'UserName': $('#UserName').val(), 'RealName': $('#RealName').val(), 'Department': $('#Department').val() }, function (data) {
+            if (data == true) {
+                alert('添加成功');
+                $('#btnClose').click();
+                document.location.reload();
+            }
+            else {
+                alert('添加失败,失败原因' + data);
+            }
+        }).fail(function () { alert('添加失败'); });
+    });
+});
